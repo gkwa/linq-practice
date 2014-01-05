56a189af74eece757d66c6b2190cb32f24250483
taylor <tailor@u.washington.edu>
2014-01-05 03:34:04 -0800

    Initial commit.  Working
    Change-Id: If0a39bb14c8b271a2b64289e9d3750c506042736

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //            XElement books = XElement.Parse(
+            //       @"<books>
+            //            <book>
+            //            <title>Pro LINQ: Language Integrated Query in C#2010</title>
+            //            <author>Joe Rattz</author>
+            //            </book>
+            //            <book>
+            //            <title>Pro .NET 4.0 Parallel Prog ramming in C#</title>
+            //            <author>Adam Freeman</author>
+            //            </book>
+            //            <book>
+            //            <title>Pro VB 2010 and the .NET 4.0 Platform</title>
+            //            <author>Andrew Troelsen</author>
+            //            </book>
+            //            </books>");
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "ConsoleApplication2.books.xml";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+
+                //   XElement books = XElement.Load(@"books.xml");
+                XElement books = XElement.Parse(result);
+                var titles =
+                from book in books.Elements("book")
+                where (string)book.Element("author") == "Joe Rattz"
+                select book.Element("title");
+                foreach (var title in titles)
+                    Console.WriteLine(title.Value);
+                Console.Write("Enter to continue...");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/README.txt
@@ -0,0 +1,2 @@
+http://www.codeproject.com/Tips/590978/LINQ-Tutorial-for-Beginners
+http://stackoverflow.com/questions/3314140/how-to-read-embedded-resource-text-file/3314213#3314213
