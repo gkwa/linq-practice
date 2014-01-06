6d5ec7f16164e9a2596b9d28a736e5f929deafde
taylor <tailor@u.washington.edu>
2014-01-05 20:28:25 -0800

    WIP: Add nmap query

--- Makefile
+++ Makefile
@@ -1,16 +1,25 @@
 RM = rm
 
-all:
+all: nmap_query
+all: ConsoleApplication2
+
+ConsoleApplication2:
 	$(MAKE) -C ConsoleApplication2
 
+nmap_query:
+	$(MAKE) -C nmap_query
+
 metaproj: ConsoleApplication2.sln.metaproj
-ConsoleApplication2.sln.metaproj: MSBuildEmitSolution=1
 ConsoleApplication2.sln.metaproj:
-	msbuild.exe
+	msbuild.exe /p:MSBuildEmitSolution=1 ConsoleApplication2.sln
 
 clean: ConsoleApplication2.sln.metaproj
 	msbuild ConsoleApplication2.sln.metaproj /t:Clean
 	$(RM) -f ConsoleApplication2.sln.metaproj
+	msbuild nmap_query.sln.metaproj /t:Clean
+	$(RM) -f nmap_query.sln.metaproj
 
 .PHONY: clean
 .PHONY: all
+.PHONY: nmap_query
+.PHONY: ConsoleApplication2
---
+++ nmap_query/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+
+
+namespace nmap_query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "nmap_query.nmap_results.xml";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+
+                ////   XElement books = XElement.Load(@"books.xml");
+                XElement nmr = XElement.Parse(result);
+
+                var query = (from host in nmr.Descendants("host")
+                             from port in host.Descendants("port")
+                             where ((string)port.Attribute("protocol") == "udp" &&
+                             (((string)port.Attribute("portid") == "1771") || ((string)port.Attribute("portid") == "1770") &&
+                             (((string)port.Element("state").Attribute("state") == "open")) || ((string)port.Element("state").Attribute("state") == "open|filtered")
+                             )) select host);
+
+                foreach (XElement address in query.Elements("address"))
+                {
+                    if ((string)address.Attribute("addrtype") == "ipv4")
+                        Console.WriteLine((string)address.Attribute("addr"));
+
+
+                }
+            }
+        }
+    }
+}
